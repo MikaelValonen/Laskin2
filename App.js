3b95bda4f2cb67fcdf762ca1020785442c09fb33
Mikael Valonen <102473112+MikaelValonen@users.noreply.github.com>
2023-09-09 18:50:12 +0300

    Github Reistailee, niin joudun palauttamaan kaiken uudestaan

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput, Button, FlatList, } from 'react-native';
+  
 export default function App() {
   const [numero1, setNumero1] = useState('');
   const [numero2, setNumero2] = useState('');
   const [vastaus, setVastaus] = useState('0');
+  const [data, setData] = useState([]);
 
   const buttonPressedplus = () => {
-    setVastaus(parseInt(numero1) + parseInt(numero2))
-  }
+    tulos = parseInt(numero1) + parseInt(numero2)
+    setVastaus(parseInt(tulos))
+    tekstina = numero1 + " + " + numero2 + " = " + parseInt(tulos)
+    if(numero1 == '' || numero2 == ''){ 
+      setVastaus(0)
+    } else {
+      setData([...data, { key: tekstina }]);
+    }
+    setNumero1('');
+    setNumero2('');
+    }
   const buttonPressedminus = () => {
-    setVastaus(numero1 - numero2)
+    tulos = numero1 - numero2
+    setVastaus(parseInt(tulos))
+    tekstina = numero1 + " + " + numero2 + " = " + parseInt(tulos)
+    if(numero1 == '' || numero2 == ''){
+      setVastaus(0)
+    } else {
+      setData([...data, { key: tekstina }]);
+    }
+    setNumero1('');
+    setNumero2('');
   }
   return (
     <View style={styles.container}>
@@ -23,6 +42,10 @@ export default function App() {
       <Button title='+' style={styles.button} onPress={() => buttonPressedplus()}>+</Button>
       <Button title='-' style={styles.button} onPress={() => buttonPressedminus()}>-</Button>
       </View>
+      <View style={styles.container}>
+      <Text>History</Text>
+      <FlatList data={data} renderItem={({item}) => <Text>{item.key}</Text>} keyExtractor={(item, index) => index.toString()} />
+      </View>
     </View>
   );
 }
@@ -30,16 +53,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 25,
+    padding: 5,
     backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
   miniContainer: {
-    flex: 0,
+    width: 150,
+    margin: 5,
+    padding: 5,
     flexDirection: 'row',
     backgroundColor: '#FFF',
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   input: {
     width: 200,
